ef2c32df01840519e63c0343461c57bbbe09f94f
Atharv Nakti <128978076+atharv3434@users.noreply.github.com>
2025-03-03 23:30:39 +0530

    Add files via upload

---
+++ Contact.java
@@ -0,0 +1,24 @@
+public class Contact {
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+    public Contact(String name, String phoneNumber, String email) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getName() { return name; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getEmail() { return email; }
+
+    public void setName(String name) { this.name = name; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return "📞 Name: " + name + " | ☎ Phone: " + phoneNumber + " | ✉ Email: " + email;
+    }
+}
